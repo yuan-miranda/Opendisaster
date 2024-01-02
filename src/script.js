9ed8c697587b3a4f1f1f63219b2694cb5fa737cb
Yuan Miranda <yuanezekielamiranda@gmail.com>
2024-01-02 20:10:40 +0800

    added menu highlighting

--- src/script.js
+++ src/script.js
@@ -1,6 +1,17 @@
 function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.toggle('active');
-    console.log("click")
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.classList.toggle('active');
+}
+
+function toggleHighlight(elementId) {
+  document.querySelectorAll('.highlighted').forEach(el => el.classList.remove('highlighted'));
+  document.getElementById(elementId).classList.add('highlighted');
+  localStorage.setItem('highlightedElement', elementId);
+}
+
+window.onload = function() {
+  const initialHighlightedElement = localStorage.getItem('highlightedElement');
+  if (initialHighlightedElement) {
+      document.getElementById(initialHighlightedElement).classList.add('highlighted');
   }
-  
+}
